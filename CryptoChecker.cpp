7c7a196a74f463d8bd633426fd5a6b367d573afe
PAOK-2001 <2001.paok@gmail.com>
2022-09-23 10:52:04 -0500

    Added custom currency. Closes #5

--- CryptoChecker.cpp
+++ CryptoChecker.cpp
@@ -14,7 +14,7 @@ curlpp::Easy request;
 Json::Reader jsonReader;
 Json::Value priceData, conversionData;
 
-float conversion_factor(string destinyCurrency){
+long conversion_factor(string destinyCurrency){
     if(destinyCurrency == "USD"){
         return 1.0;
     }
@@ -28,11 +28,11 @@ float conversion_factor(string destinyCurrency){
     return stod(conversionData["result"].asString());
 }
 
-bool sortbysec(const pair<string,float> &a, const pair<string,float> &b){
+bool sortbysec(const pair<string,long> &a, const pair<string,long> &b){
     return (a.second > b.second);
 }
 
-void sortOperation(vector<pair<string, float>> &cryptos){
+void sort_operation(vector<pair<string, long>> &cryptos){
     sort(cryptos.begin(), cryptos.end(), sortbysec);
 }
 
@@ -51,7 +51,7 @@ vector<string> spliter(string rawQuery)
 }
 
 // API Queries
-vector<pair<string, float>> get_price(vector<pair<string, float>>& returnVec, string search = " ", string convert ="USD")
+vector<pair<string, long>> get_price(vector<pair<string, long>>& returnVec, string search = " ", string convert ="USD")
 {
     stringstream stringQuery;
     const string key = "https://api.binance.com/api/v3/ticker/price?symbol=";
@@ -66,7 +66,7 @@ vector<pair<string, float>> get_price(vector<pair<string, float>>& returnVec, st
         jsonReader.parse(stringQuery, priceData);
         returnVec.push_back(make_pair(crypto, stod(priceData["price"].asString())*conversion_factor(convert)));
     }
-    sortOperation(returnVec);
+    sort_operation(returnVec);
     return returnVec;
 }
 // Command functions
@@ -75,13 +75,15 @@ void help_command()
     cerr<<endl<<BOLD(GREEN("Welcome to Crypto Checker!!"))<<endl;
     cerr<<"-----------------------------\n"
     "The command scheme is as follows\n";
-    cerr<<BOLD(BLUE("-h"))<<"    Print out help menu\n";
-    cerr<<BOLD(BLUE("-t"))<<"    See 7 most popular cryptos in USD\n";
-    cerr<<BOLD(BLUE("-s"))<<"    Search for a crypto or cryptos separates with commas and no space"<<endl<<endl;
+    cerr<<BOLD(BLUE("-h"))<<BOLD("| --help")<<"      Print out help menu"<<endl;
+    cerr<<BOLD(BLUE("-t"))<<BOLD("| --top")<<"       See 7 most popular cryptos in USD"<<endl;
+    cerr<<BOLD(BLUE("-s"))<<BOLD("| --search")<<"    Search for a one or multiple cryptos separated with commas and no space. Specify the  abreviation ie. BTC"<<endl;
+    cerr<<BOLD(BLUE("-c"))<<BOLD("| --customcurreny")<<" See the top cryptos in a specified currency, specified after flag. Specify abreviation ie. MXN"<<endl<<endl;
+
 }
 
-void topCommand(){
-    vector<pair<string, float>> defaultResult;
+void top_command(){
+    vector<pair<string, long>> defaultResult;
     get_price(defaultResult);
     cerr<<endl<<BOLD(BLUE("CRYPTO CURRENY"))<<BOLD(" | ")<<BOLD(BLUE("PRICE \n"));
     cerr<<BOLD("-----------------------\n");
@@ -102,7 +104,7 @@ void topCommand(){
 }
 
 void search_command(string search){
-    vector<pair<string, float>> defaultResult;
+    vector<pair<string, long>> defaultResult;
     get_price(defaultResult, search);
     cerr<<endl<<BOLD(BLUE("CRYPTO CURRENY"))<<BOLD(" | ")<<BOLD(BLUE("PRICE \n"));
     cerr<<BOLD("-----------------------\n");
@@ -112,24 +114,46 @@ void search_command(string search){
     cerr<<endl;
 }
 
+void custom_currency(string currency){
+    vector<pair<string, long>> defaultResult;
+    get_price(defaultResult," ",currency);
+    cerr<<endl<<BOLD(BLUE("CRYPTO CURRENY"))<<BOLD(" | ")<<BOLD(BLUE("PRICE \n"));
+    cerr<<BOLD("-----------------------\n");
+    int i = 0;
+    for(auto& result: defaultResult){
+        if(i == 0)
+        {
+            cerr<<result.first<<BOLD("        | ")<<KGRN<<result.second<<RST<<endl;
+        }else if(i==(defaultResult.size()-1)){
+            cerr<<result.first<<BOLD("        | ")<<KRED<<result.second<<RST<<endl;
+        }
+        else{
+            cerr<<result.first<<BOLD("        | ")<<result.second<<endl;
+        }
+        i++;
+    }
+    cerr<<endl;
+}
+
 int main(int argc, char*argv[]){
     string flagValue;
     //Parse flags
-    switch (getopt(argc,argv,"hts:m:"))
+    switch (getopt(argc,argv,"hts:c:"))
     {
     case 'h':
         help_command();
         break;
-
     case 't':
-        topCommand();
+        top_command();
         break;
-
     case 's':
         flagValue = optarg;
         search_command(flagValue);
         break;
-
+    case 'c':
+        flagValue = optarg;
+        custom_currency(flagValue);
+        break;
     default:
         help_command();
         break;
